eb287147410f54299765fda369a84b7a3cf453ff
Yorick van der Vis <yorick@fastmail.com>
2022-05-19 22:50:47 +0200

    Simple structure for Map and Player

---
+++ src/map_builder.rs
@@ -0,0 +1,16 @@
+use crate::prelude::*;
+
+CONST NUM_ROOMS: usize = 20;
+
+
+pub struct MapBuilder{
+    pub map: Map,
+    pub rooms: Vec<Rect>,
+    pub player_start: Point,
+}
+
+impl MapBuilder {
+    fn fill(&mut self, tile: TileType){
+        self.map.tiles.iter_mut().for_each(|t| *t = tile);
+    }
+}
